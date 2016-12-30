c40d12a49f34d4f2cdffd8d78606591f6547ca0a
tyler <yohamsandwich@gmail.com>
2016-12-30 15:45:07 -0500

    added, add new note function.

--- app/src/main/java/omar/notekeeping/NotePaper.java
+++ app/src/main/java/omar/notekeeping/NotePaper.java
@@ -2,6 +2,10 @@ package omar.notekeeping;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import omar.notekeeping.SaveLoad;
 
 public class NotePaper extends AppCompatActivity
 {
@@ -10,5 +14,13 @@ public class NotePaper extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_paper);
+    }
+    public void saveButton(View view)
+    {
+        SaveLoad sl = new SaveLoad ();
+        EditText titlet = (EditText)findViewById(R.id.Title);
+        EditText NotePadet = (EditText)findViewById(R.id.NotePad);
+        String titletext = titlet.getText().toString();
+        String notepad = NotePadet.getText().toString();
     }
 }
